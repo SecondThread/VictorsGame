d2955087e71e359316f972a03b0896e5fcdae54d
WumboGames <mywumbogames@gmail.com>
2016-07-15 15:37:03 -0500

    added a bunch of networking stuff, this is going to be impossible...

---
+++ src/engine/networking/MultiServer.java
@@ -0,0 +1,24 @@
+package engine.networking;
+import java.io.*;
+import java.net.ServerSocket;
+
+public class MultiServer {
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket=null;
+		boolean listening=true;
+		short port=7555;
+
+		try {
+			serverSocket=new ServerSocket(port);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: "+port+"...");
+			System.exit(-1);
+		}
+
+		while (listening) {
+			new MultiServerThread(serverSocket.accept()).start();
+		}
+
+		serverSocket.close();
+	}
+}
---
+++ src/engine/networking/MultiServerThread.java
@@ -0,0 +1,67 @@
+package engine.networking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MultiServerThread extends Thread {
+	private Socket socket=null;
+
+	public MultiServerThread(Socket socket) {
+		super("MultiServer Thread");
+		this.socket=socket;
+	}
+
+	public void run() {
+
+		try {
+			PrintWriter out=new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String inputLine;
+			ConcurrentHashMap<String, String> map=new ConcurrentHashMap<String, String>();
+			
+			while ((inputLine=in.readLine())!=null) {
+				String[] arguements=inputLine.split(" ");
+				String command=arguements[0];
+				if (command.equalsIgnoreCase("leave")) {
+					out.println("ACK, bye");
+					break;
+				}
+				if (command.equalsIgnoreCase("set")) {
+					String name=arguements[1], value=arguements[2];
+					map.put(name, value);
+					out.println("ACK, set");
+				}
+				else {
+					if (command.equalsIgnoreCase("get")) {
+						String name=arguements[1];
+						if (map.containsKey(name)) {
+							out.println("ACK: "+map.get(name));
+						}
+						else {
+							if (arguements.length>2) {
+								out.println("ACK: "+arguements[2]);
+							}
+							else {
+								out.println("NAC: Key Not Found");
+							}
+							
+						}
+					}
+					else {
+						out.println("NAC");
+					}
+				}
+			}
+			out.close();
+			in.close();
+			socket.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
